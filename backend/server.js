4f9b1b3b15ba4e0ce050e845df8c372b57ee06a2
challatoor murali <challatoor murali.gmail.com>
2026-08-12 16:35:48 +0530

    git_actions

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,4 @@
-// import path from 'path'
-// import express from 'express'
-
-// import dotenv from 'dotenv'
-
-// import connectDB from './config/db.js'
-// import productRoutes from './routes/productRoutes.js'
-// import userRoutes from './routes/userRoutes.js'
-// import uploadRoutes from './routes/uploadRoutes.js'
-
-// dotenv.config()
-// connectDB()
-
-// const app = express()
-// app.use(express.json())
-// app.use('/api/products', productRoutes)
-// app.use('/api/users', userRoutes)
-// app.use('/api/uploads', uploadRoutes)
-
-// app.get('/api/config/cloudinary', (req, res) => {
-//   res.send(process.env.CLOUDINARY_URL)
-// })
-// app.get('/api/config/cloudinarypreset', (req, res) => {
-//   res.send(process.env.CLOUDINARY_UPLOAD_PRESET)
-// })
-
-// const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'Frontend' ,'build')))
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running...')
-//   })
-// }
-
-// app.use((err, req, res, next) => {
-//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-//   res.status(statusCode)
-//   res.json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-//   })
-// })
-
-// const PORT = process.env.PORT || 5000
-// app.listen(PORT, console.log(`Server is running on port ${PORT}`))
-
-
-
-
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -62,13 +9,15 @@ import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
+// Connect to MongoDB
 connectDB()
 
 const app = express()
 
+// Middleware
 app.use(express.json())
 
-// API routes
+// API Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/uploads', uploadRoutes)
@@ -82,7 +31,7 @@ app.get('/api/config/cloudinarypreset', (req, res) => {
     res.send(process.env.CLOUDINARY_UPLOAD_PRESET)
 })
 
-// Uploads
+// Static uploads
 const __dirname = path.resolve()
 
 app.use(
@@ -90,7 +39,7 @@ app.use(
     express.static(path.join(__dirname, 'uploads'))
 )
 
-// Backend health/API endpoint
+// Backend health check
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
