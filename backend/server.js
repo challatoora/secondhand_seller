d77f2cb73ad059939a0534c088a72993746c725b
challatoor murali <challatoor murali.gmail.com>
2026-08-12 16:28:29 +0530

    git_actions

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,7 @@
 
 
 
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -67,40 +68,40 @@ const app = express()
 
 app.use(express.json())
 
-// API routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/uploads', uploadRoutes)
 
-// Cloudinary configuration
 app.get('/api/config/cloudinary', (req, res) => {
-  res.send(process.env.CLOUDINARY_URL)
+    res.send(process.env.CLOUDINARY_URL)
 })
 
 app.get('/api/config/cloudinarypreset', (req, res) => {
-  res.send(process.env.CLOUDINARY_UPLOAD_PRESET)
+    res.send(process.env.CLOUDINARY_UPLOAD_PRESET)
 })
 
-// Uploaded files
-app.use('/uploads', express.static('uploads'))
+const __dirname = path.resolve()
 
-// Backend health check
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+// Backend health/API endpoint
 app.get('/', (req, res) => {
-  res.send('API is running...')
+    res.send('API is running...')
 })
 
-// Error handler
 app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
 
-  res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production'
+            ? null
+            : err.stack,
+    })
 })
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
